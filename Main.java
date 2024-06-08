6b4665a7ebffd7c65071010b39cee685a619fc5f
Daylen Hendricks <131936800+DaylenHendricks@users.noreply.github.com>
2024-06-08 15:07:45 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,27 @@
+/* COP 3503C Assignment 2
+This program is written by: Daylen Hendricks */
+
+import java.util.Scanner;
+import java.util.ArrayList;;
+
 public class Main {
-    
+    public char[] targetWord;
+    public char[] foundWord;
+}
+
+class element {     //the elements of the matrix
+    private int letter;
+    private boolean used;//true if already in found word
+    private boolean marked;//letter isn't in target word
+
+    public element(char letter){
+        this.letter = letter;
+    }
+    public boolean checkMatch(char letter){
+        if(this.letter == letter)
+            return true;
+        else
+            return false;
+    }
+
 }
