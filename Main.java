e379c6059337e1e6ae9c2cdff52b112e28092ef5
Daylen Hendricks <131936800+DaylenHendricks@users.noreply.github.com>
2024-06-09 15:52:02 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -14,13 +14,18 @@ public class Main {
         int n = scan.nextInt();//columns
         int s = scan.nextInt();//# words to find
         String input;//current scanner line
-        char[][] matrix = new char[m][n];//letter matrix
+        Element[][] matrix = new Element[m][n];//letter matrix
+        char[][] solutionMatrix = new char[m][n];
         ArrayList<String>[] searchWords = new ArrayList[s];//words to find
+
+        for (int i = 0; i < s; i++) {
+            searchWords[i] = new ArrayList<>();
+        }
 
         for(int i = 0; i < m; i++){//build matrix
             input = scan.nextLine();
             for(int j = 0; j < n; j++){
-                matrix[i][j] = input.charAt(j);
+                matrix[i][j] = new Element(input.charAt(j));
             }
         }
 
@@ -29,6 +34,41 @@ public class Main {
                 searchWords[i].add(str);
         }
 
+
+        for(int i = 0; i < s; i++){
+            String targetWord = searchWords[i].get(0);
+            findWord(matrix, targetWord, solutionMatrix);
+        }
+
+
+    scan.close();//close scanner
+    }
+
+    public static boolean findWord(Element[][] matrix, String targetWord, char[][] solutionMatrix){
+            if (findAlg(matrix, 0, 0, targetWord, 0, solutionMatrix) == false) {
+                System.out.print("Solution doesn't exist");
+                return false;
+            }
+            else{
+                // printSolution(char[][] solutionMatrix, int rows, int columns;
+            return true;
+            }
+    }
+
+    public static boolean findAlg(Element[][] matrix, int x, int y, String targetWord, int targetIndex, char[][] solutionMatrix){
+        boolean solutionPossible = true;
+        // String target = searchWords.get(0);
+        while (solutionPossible){
+            if(targetIndex == targetWord.length() && matrix[x][y].getLetter() == targetWord.charAt(targetWord.length())){//best case, solution
+                return true;
+            }
+            else{//if not solution
+                if(matrix[x][y].getLetter() == targetWord.charAt(targetIndex)){
+
+                }
+            }
+        }
+        return false;//default, but change before submitting
     }
 
     public static void printSolution(char[][] solutionMatrix, int rows, int columns){
@@ -45,15 +85,18 @@ public class Main {
     }
 }
 
-class element {//the elements of the matrix
-    private int letter;
+class Element {//the elements of the matrix
+    private char letter;
     private boolean used;//already in found word
     private boolean marked;//isn't in target word
 
-    public element(char letter){
+    public Element(char letter){
         this.letter = letter;
         this.used = false;
         this.marked = false;
+    }
+    public char getLetter(){
+        return this.letter;
     }
     public boolean checkMatch(char letter){
         if(this.letter == letter)
@@ -73,5 +116,4 @@ class element {//the elements of the matrix
     public void use(){
         this.used = true;
     }
-
 }
